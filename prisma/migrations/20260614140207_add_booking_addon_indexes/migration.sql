3085631592242969dbd3dad93d61f124795755ef
imsadegh <sadiq904@icloud.com>
2026-06-14 17:32:09 +0330

    fix: add indexes on BookingAddon FK columns, rename addon relation field

---
+++ prisma/migrations/20260614140207_add_booking_addon_indexes/migration.sql
@@ -0,0 +1,5 @@
+-- CreateIndex
+CREATE INDEX "booking_addons_bookingId_idx" ON "booking_addons"("bookingId");
+
+-- CreateIndex
+CREATE INDEX "booking_addons_addonId_idx" ON "booking_addons"("addonId");
